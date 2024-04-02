4aba87741e255f809c9b348f8fe7da44c65520a6
Jeremy Zhao <jeremy.zhao@softcat.cn>
2024-04-02 12:24:26 +0800

    npm发布

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm'],
   splitting: true,
   sourcemap: true,
   clean: true,
-  minify: true,
   dts: 'src/index.ts',
-  outDir: './dist'
+  outDir: './dist',
 })
